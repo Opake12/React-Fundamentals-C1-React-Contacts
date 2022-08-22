3ac870c308ce45240c98c7c607f3745037bd75f7
Robert Zivkovic <54717929+Opake12@users.noreply.github.com>
2022-08-22 16:48:46 +0200

    before state

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,36 @@
-import { useState, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
-import CreateContact from "./CreateContact.js";
-import * as ContactsAPI from "../utils/ContactsAPI";
+
+
 
 const App = () => {
-  let navigate = useNavigate();
-
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => {
-    const getContacts = async () => {
-      const res = await ContactsAPI.getAll();
-      setContacts(res);
-    };
-
-    getContacts();
-  }, []);
-
-  const removeContact = (contact) => {
-    ContactsAPI.remove(contact);
-
-    setContacts(contacts.filter((c) => c.id !== contact.id));
-  };
-
-  const createContact = (contact) => {
-    const create = async () => {
-      const res = await ContactsAPI.create(contact);
-      setContacts(contacts.concat(res));
-    };
-
-    create();
-    navigate("/");
-  };
-
+  const contacts = [
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:3000/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:3000/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:3000/tyler.jpg",
+    },
+  ];
   return (
-    <Routes>
-      <Route
-        exact
-        path="/"
-        element={
-          <ListContacts contacts={contacts} onDeleteContact={removeContact} />
-        }
-      />
-      <Route
-        path="/create"
-        element={
-          <CreateContact
-            onCreateContact={(contact) => {
-              createContact(contact);
-            }}
-          />
-        }
-      />
-    </Routes>
-  );
+    <div>
+      <ListContacts contacts={ contacts }/>
+    </div>
+  )
 };
+
 
 export default App;
