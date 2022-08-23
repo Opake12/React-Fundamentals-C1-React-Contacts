0938de0430ea1dac0b5ef0343101f1aaf3d786f1
Robert Zivkovic <54717929+Opake12@users.noreply.github.com>
2022-08-23 15:11:21 +0200

    up to controlled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
 
 
-
 const App = () => {
-  const contacts = [
+  const removeContact = (contact) => {
+    setContacts(contacts.filter(c => c.id !== contact.id))
+  }
+
+  const [contacts, setContacts] = useState([
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "@tylermcginnis",
+      avatarURL: "http://localhost:3001/tyler.jpg",
+    },
     {
       id: "karen",
       name: "Karen Isgrigg",
-      handle: "karen_isgrigg",
-      avatarURL: "http://localhost:3000/karen.jpg",
+      handle: "@karen_isgrigg",
+      avatarURL: "http://localhost:3001/karen.jpg",
     },
     {
       id: "richard",
       name: "Richard Kalehoff",
-      handle: "richardkalehoff",
-      avatarURL: "http://localhost:3000/richard.jpg",
+      handle: "@thehoff",
+      avatarURL: "http://localhost:3001/hoff.jpg",
     },
     {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "tylermcginnis",
-      avatarURL: "http://localhost:3000/tyler.jpg",
+      id: "foo",
+      name: "foochard Kalehoff",
+      handle: "@thefoo",
+      avatarURL: "http://localhost:3001/foo.jpg",
     },
-  ];
+  ]);
+    
   return (
     <div>
-      <ListContacts contacts={ contacts }/>
+      <ListContacts contacts={ contacts } onDeleteContact={removeContact} />
     </div>
-  )
+  );
 };
 
 
